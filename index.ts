8f808c41104302f71299847fce934d0d27953246
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:42:20 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,23 @@
+//   Q15:Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+//  Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+// • Print a second set of invitation messages, one for each person who is still in your list.
+
+
+ 
+ let guestArr: string[] = ["junaid", "Ahmer", "kamran", "MISS NIDA"];
+ let canNotAttend: string = "junaid"
+ console.log(canNotAttend + " " + "can not make it, for Dinner");
+
+ 
+ let newGuest: string = "Hamza"
+ guestArr[guestArr.indexOf(canNotAttend)] =newGuest;
+ console.log(guestArr)
+
+
+guestArr.map((item) =>
+console.log(`Dear ${item}, you are cordinally invited to a dinner`)
+)
